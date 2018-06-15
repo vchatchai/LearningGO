99c71268c767e76d2b4810a79ff00cdd611f745d
Chatchai Vichai <ee56054@gmail.com>
2018-06-15 17:16:18 +0700

    slice copy

---
+++ src/slice/copy_test.go
@@ -0,0 +1,16 @@
+package slice
+
+import "testing"
+
+func TestCopy(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{"Test copy"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Copy()
+		})
+	}
+}
